3155694550e77d439e1c7a5a2778fb058da9df6c
eric <eric@DESKTOP-JL0IH99>
2017-03-25 22:15:55 -0400

    changed trivial thigns

--- sheet-music-recognition/sheet-music-recognition/StaffClassifier.py
+++ sheet-music-recognition/sheet-music-recognition/StaffClassifier.py
@@ -2,18 +2,20 @@ import cv2
 import sys
 import numpy as np
 
-image_path = sys.argv[1]
-template_path = sys.argv[2]
+def find_notes():
+    image_path = sys.argv[1]
+    template_path = sys.argv[2]
 
-img_rgb = cv2.imread(image_path)
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread(template_path,0)
-w, h = template.shape[::-1]
+    img_rgb = cv2.imread(image_path)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(template_path,0)
+    w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.5
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.95
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-cv2.imwrite('res.png',img_rgb)
+    cv2.imwrite('Images/res.png',img_rgb)
+
