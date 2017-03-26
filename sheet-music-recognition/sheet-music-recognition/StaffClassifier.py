ccad848b98838d65be2a847598f74a83b29ef3e7
Jacob <jacobgweisberg@gmail.com>
2017-03-25 22:22:59 -0400

    renamed image

--- sheet-music-recognition/sheet-music-recognition/StaffClassifier.py
+++ sheet-music-recognition/sheet-music-recognition/StaffClassifier.py
@@ -13,14 +13,14 @@ w_img, h_img = img_gray.shape[::-1]
 
 if (w_img < w) or (h_img < h):
     if (h - h_img) >= (w - w_img):
-        h_note = 0.10 * h_img
+        h_note = 0.11 * h_img
         scalefactor = h_note / h
         res = cv2.resize(template, None, fx=scalefactor, fy=scalefactor, interpolation = cv2.INTER_AREA)
     else:
         print "Please use a template image with width less than %d" % w_img
 
 res = cv2.matchTemplate(img_gray,res,cv2.TM_CCOEFF_NORMED)
-threshold = 0.9
+threshold = 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
